621726726577d47d483007520ce3118d0c6068ea
Gleb Gerlakh <ggerlah55@gmail.com>
2024-11-23 02:58:30 +0300

    Add png support

--- hw_2/README.md
+++ hw_2/README.md
@@ -2,4 +2,22 @@
 
 Выполнение второго ДЗ
 
+## Создание виртуального окружения
+
+1. Окружение создается через `conda`
+
+   ```bash
+   conda create -n hw_2 -c conda-forge python=3.12 pdflatex 
+   ```
+2. Для активации окружения используется следующая команда:
+   
+   ```bash
+   conda activate hw_2
+   ```
+3. Для деактивации:
+
+   ```bash
+   conda deactivate
+   ```
+
 ## Модуль convert_to_tex.py
--- hw_2/convert_to_tex.py
+++ hw_2/convert_to_tex.py
@@ -1,8 +1,14 @@
+import os
+import pathlib
+import subprocess
 from typing import List
 
 
-def generate_table(inp_list: List[List[any]]) -> str:
+def generate_tex_doc(tex_content: str) -> str:
+    return "\\documentclass{article}\n" + "\\usepackage{graphicx}\n" + "\\begin{document}\n" + tex_content + "\end{document}\n"
 
+
+def generate_table(inp_list: List[List[any]]) -> str:
     tex_table_cells_alignment = "|"
     for i in range(len(inp_list[0])):
         tex_table_cells_alignment += "c|"
@@ -13,4 +19,18 @@ def generate_table(inp_list: List[List[any]]) -> str:
         tex_table_row += "\\hline\n"
         tex_table_str += tex_table_row
     tex_table_str += "\\end{tabular}\n"
-    return "\\documentclass{article}\n" + "\\usepackage{graphicx}\n" + "\\begin{document}\n" + tex_table_str + "\end{document}\n"
+    return tex_table_str
+
+
+def generate_picture(pic_png_path: str) -> str:
+    dirname = str(pathlib.Path(pic_png_path).parent.resolve())
+    filename = pathlib.Path(pic_png_path).stem
+    return "\\graphicspath{ {" + dirname + "} }\n" + "\n\\includegraphics{" + filename + "}\n"
+
+
+def convert_tex_to_pdf(path_to_tex: str) -> None:
+    dirname = str(pathlib.Path(path_to_tex).parent.resolve())
+    proc = subprocess.run(f"pdflatex -output-directory={dirname} {path_to_tex}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    proc.check_returncode()
+    os.remove(path_to_tex[:-4] + ".log")
+    os.remove(path_to_tex[:-4] + ".aux")
