0e4736e8dee5edabad84915b74c848891fe8a8c0
Gleb Gerlakh <ggerlah55@gmail.com>
2024-11-23 20:38:47 +0300

    add latex-gen-ggerlakh

---
+++ hw_2/src/latex_gen_ggerlakh/example.py
@@ -0,0 +1,17 @@
+from latex_gen_ggerlakh.convert_to_tex import generate_table, generate_tex_doc, generate_picture, convert_tex_to_pdf
+
+
+if __name__ == "__main__":
+    path_to_output_tex_file = "../../artifacts/table_and_png.tex"
+    path_to_png = "../../artifacts/python.png"
+    with open(path_to_output_tex_file, "w") as tex_file:
+        l = [
+            ["id", "val", "num", "group"],
+            [7, 3, 9, 1],
+            [5, True, None, 8],
+            ["k", 6, "ABC", 3]
+            ]
+        tex_file.write(generate_tex_doc(generate_table(l) + generate_picture(path_to_png)))
+        print(f"write tex output to {path_to_output_tex_file}")
+    convert_tex_to_pdf(path_to_output_tex_file)
+    print(f"converted {path_to_output_tex_file} to {path_to_output_tex_file[:-4] + '.pdf'}")
